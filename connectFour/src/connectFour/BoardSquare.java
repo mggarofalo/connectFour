e349957b2052fc2cedd7ae10fac05758084e992b
Michael <Michael@Michael>
2017-07-31 11:23:39 -0400

    Added BoardSquare methods.
    Now we can call methods on the BoardSquare to move it in any direction.

--- connectFour/src/connectFour/BoardSquare.java
+++ connectFour/src/connectFour/BoardSquare.java
@@ -2,8 +2,8 @@ package connectFour;
 
 public class BoardSquare {
 
-	private final int col;
-	private final int row;
+	private int col;
+	private int row;
 
 	public BoardSquare(int aCol, int aRow) {
 		col = aCol;
@@ -18,4 +18,29 @@ public class BoardSquare {
 		return row;
 	}
 
+	// This doesn't need c/r input, but it'll read more easily if I require it
+	public void moveLeft(int c, int r) {
+		if (col != 0) {
+			col -= 1;
+		}
+	}
+
+	public void moveRight(int c, int r) {
+		if (col < c) {
+			col += 1;
+		}
+	}
+
+	// This doesn't need c/r input, but it'll read more easily if I require it
+	public void moveDown(int c, int r) {
+		if (row != 0) {
+			row -= 1;
+		}
+	}
+
+	public void moveUp(int c, int r) {
+		if (row < r) {
+			row += 1;
+		}
+	}
 }
