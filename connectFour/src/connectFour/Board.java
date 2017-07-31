8743b39196b92ce7e994ea6159154a620636e501
Michael <Michael@Michael>
2017-07-31 12:04:25 -0400

    Changed int[][] from 0-base to 1-base.
    The 0-base vs. 1-base display was ripe for confusion. This gets rid of
    that issue.

--- connectFour/src/connectFour/Board.java
+++ connectFour/src/connectFour/Board.java
@@ -21,8 +21,9 @@ public class Board {
 
 	public Board(int w, int h) {
 		// Set the width and height variables
-		width = w;
-		height = h;
+		// Note that we're converting from 0-base to 1-base here to avoid confusion
+		width = w + 1;
+		height = h + 1;
 
 		// Initialize a board to the width and height
 		boardArray = new int[width][height];
@@ -40,12 +41,12 @@ public class Board {
 	public int[][] readBoard() {
 		return boardArray;
 	}
-	
+
 	// Returns the width of the board
 	public int readWidth() {
 		return width;
 	}
-	
+
 	// Return the height of the board
 	public int readHeight() {
 		return height;
@@ -64,7 +65,7 @@ public class Board {
 
 	// Gets the lowest open row for a given column
 	public int getLowestRow(int c) {
-		for (int r = 0; r < height; r++) {
+		for (int r = 1; r < height; r++) {
 			if (boardArray[c][r] == -1) {
 				return r;
 			}
@@ -82,24 +83,24 @@ public class Board {
 	// Prints the board to the console
 	public void printBoard() {
 		// Print board contents to look like this:
-		// Note: stupid Eclipse auto-format doesn't keep leading spaces, hence ///
+		// Note: Eclipse auto-format doesn't keep leading spaces, hence ///
 		/// 1 2 3 4 5 6 7
 		// | | | | | | | |
 		// | | | | | | | |
 		// |1| |2|1|1|2| |
 
 		// Print column labels
-		for (int c = 0; c < width; c++) {
+		for (int c = 1; c < width; c++) {
 			System.out.print(" ");
 			System.out.print(c);
 		}
 
-		for (int r = height - 1; r >= 0; r--) {
+		for (int r = height - 1; r >= 1; r--) {
 			// Initial border
 			System.out.print("|");
 
 			// Loop through columns in row
-			for (int c = 0; c < width; c++) {
+			for (int c = 1; c < width; c++) {
 				// Print blank space if the space is empty; otherwise print the player number.
 				if (boardArray[c][r] == -1) {
 					System.out.print(" ");
